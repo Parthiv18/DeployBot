ff71e3cca92c3f36de9c01f25ec2c13bcb7a7d54
Parthiv18 <parthiv675@gmail.com>
2022-01-30 15:43:11 -0500

    Fixed 3 times sending

--- src/commands.js
+++ src/commands.js
@@ -3,9 +3,9 @@ const task = require("./stocks");
 
 module.exports = async function (msg) {
   if (msg.content.toUpperCase() === "Hello DeployBot".toUpperCase()) {
-    msg.reply("Welcome Person w");
+    msg.channel.send("Welcome Person w");
   } else if (msg.content.toUpperCase() === "Gay".toUpperCase()) {
-    msg.reply("Yes you are gay");
+    msg.channel.send("Yes you are gay");
   }
 
   //8ball
@@ -18,16 +18,16 @@ module.exports = async function (msg) {
   ];
   var num = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
   if (msg.content.startsWith("-8ball")) {
-    msg.reply(rep[num]);
+    msg.channel.send(rep[num]);
   }
 
   //stocks
   if (msg.content.startsWith("-stocks") || msg.content.startsWith("-stock")) {
     var stockName = msg.content.split(" ");
     if (stockName.length === 2) {
-      task.findStocks(stockName[1]).then((x) => msg.reply(x));
+      task.findStocks(stockName[1]).then((x) => msg.channel.send(x));
     } else {
-      msg.reply("Cannot Comprehend");
+      msg.channel.send("Cannot Comprehend");
     }
   }
 };
