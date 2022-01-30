0aec8a56221261f4712b97cd5ccacc7a5078ca25
Parthiv18 <parthiv675@gmail.com>
2022-01-30 12:17:14 -0500

    first commit

---
+++ src/SendMsg.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = async function (msg) {
+  if (msg.content.toUpperCase() === "Hello DeployBot".toUpperCase()) {
+    msg.reply("Welcome");
+  }
+
+  //8ball
+  const rep = [
+    "As I see it, yes!",
+    "Ask again later!",
+    "Concentrate and ask again!",
+    "It is decidedly so!",
+    "Don't count on it!",
+    "My sources say no",
+  ];
+  var num = Math.floor(Math.random() * 6);
+  if (msg.content.startsWith("-8ball")) {
+    msg.reply(rep[num]);
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const commandHandler = require("./SendMsg");
+
+
+bot.login(TOKEN);
+
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", commandHandler);
