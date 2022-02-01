08fce7712679be7122e41e43b07bc260bd1f528b
Parthiv18 <parthiv675@gmail.com>
2022-01-31 19:40:05 -0500

    fixed sizing and added more characters

--- src/games.js
+++ src/games.js
@@ -21,21 +21,30 @@ module.exports = async function (msg) {
     if (animeName.length === 2) {
       //Anime [Demon Slayer]
       if (animeName[1].toUpperCase() === "DemonSlayer".toUpperCase()) {
-        var picPicker = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
+        var picPicker =
+          Math.floor(
+            Math.random() * (anime.DemonSlayerCharacters.length - 0 + 1)
+          ) + 0;
         msg.reply("You Got: ", {
           files: [anime.DemonSlayerCharacters[picPicker]],
         });
       }
       //Anime [Naruto]
       else if (animeName[1].toUpperCase() === "naruto".toUpperCase()) {
-        var picPicker = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
+        var picPicker =
+          Math.floor(Math.random() * (anime.NarutoCharacters.length - 0 + 1)) +
+          0;
         msg.reply("You Got: ", {
           files: [anime.NarutoCharacters[picPicker]],
         });
       }
       //Anime [AOT]
-      else if (animeName[1].toUpperCase() === "attackontitan".toUpperCase() || animeName[1].toUpperCase() === "aot".toUpperCase()) {
-        var picPicker = Math.floor(Math.random() * (21 - 0 + 1)) + 0;
+      else if (
+        animeName[1].toUpperCase() === "attackontitan".toUpperCase() ||
+        animeName[1].toUpperCase() === "aot".toUpperCase()
+      ) {
+        var picPicker =
+          Math.floor(Math.random() * (anime.AOTCharacters.length - 0 + 1)) + 0;
         msg.reply("You Got: ", {
           files: [anime.AOTCharacters[picPicker]],
         });
--- src/pictures/pics.js
+++ src/pictures/pics.js
@@ -11,6 +11,14 @@ exports.DemonSlayerCharacters = [
   "https://i.pinimg.com/474x/28/5e/6e/285e6e3c1053f9d9f945cb64483845ff.jpg",
   "https://i.pinimg.com/474x/8b/78/a1/8b78a1be93f89fefc8eb4521749e8350.jpg",
   "https://i.pinimg.com/474x/65/cb/53/65cb53b78f7f36088032598fd4a5f10a.jpg",
+  "https://i.pinimg.com/474x/28/3b/79/283b795a57cb3434a7b85e34bd4c1c3c.jpg",
+  "https://i.pinimg.com/474x/f7/9d/2f/f79d2fc15af1e037c0261b91ad7129c5.jpg",
+  "https://i.pinimg.com/474x/01/4a/70/014a70fea65148a3080025fbf700a946.jpg",
+  "https://i.pinimg.com/474x/58/62/57/586257bc0934cd5661d5bc0445b529be.jpg",
+  "https://i.pinimg.com/474x/ae/c2/20/aec220d9a616807e302e04d161e1be52.jpg",
+  "https://i.pinimg.com/474x/1c/f6/97/1cf697466cd21bede2651b5fa9106c2b.jpg",
+  "https://i.pinimg.com/474x/7b/7f/d3/7b7fd35eebe28f4795ced1af35496d12.jpg",
+  "https://i.pinimg.com/474x/fa/60/0a/fa600af3229d7948726b19b305c920c4.jpg",
 ]; //size 11
 
 exports.NarutoCharacters = [
@@ -31,7 +39,18 @@ exports.NarutoCharacters = [
   "https://i.pinimg.com/474x/6b/70/53/6b70538d10bb8782308ca52b1919ac00.jpg",
   "https://i.pinimg.com/474x/48/13/e5/4813e5872edfa95a03909a21c0b9efce.jpg",
   "https://i.pinimg.com/474x/ad/35/81/ad3581384b9dcbd65639f107e95fa1cc.jpg",
-]; //size 16
+  "https://i.pinimg.com/474x/a6/7f/d9/a67fd9301ef563689da61150e9577fa1.jpg",
+  "https://i.pinimg.com/474x/6a/1c/43/6a1c43bfc30e04848ef60ed6dc876a34.jpg",
+  "https://i.pinimg.com/474x/75/13/29/75132981e6d864cf6fbc32da6072f52f.jpg",
+  "https://i.pinimg.com/474x/10/3c/b6/103cb65be0e1238bfe32908d4f0b4c38.jpg",
+  "https://i.pinimg.com/474x/89/95/ff/8995ffe7a025a11956126a46a6791099.jpg",
+  "https://i.pinimg.com/474x/b3/d7/5a/b3d75a5cafa5a67ff9b39b086a218989.jpg",
+  "https://i.pinimg.com/474x/3d/dc/82/3ddc82a0eb7d3d68a9c09ce9a6b010ff.jpg",
+  "https://i.pinimg.com/474x/e5/3b/f0/e53bf06e405709b2d6a3e910253474ba.jpg",
+  "https://i.pinimg.com/474x/63/bb/e7/63bbe7f2a6187d8484d874df904efa12.jpg",
+  "https://i.pinimg.com/474x/37/ed/7b/37ed7b730624dd1a596e48aaec26791d.jpg",
+  "https://i.pinimg.com/474x/62/b9/8b/62b98b73c5575c39fe582867c03308f1.jpg"
+]; //size 
 
 exports.AOTCharacters = [
   "https://i.pinimg.com/564x/2f/c9/d1/2fc9d136bef6d5d2979c0a892bd68c5b.jpg",
